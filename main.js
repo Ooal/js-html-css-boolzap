9fbafb8f787e68d6d4cec882d21b037143e7b88e
Enrico <enricosellanora96@gmail.com>
2020-07-13 16:07:22 +0200

    Domand e risposta

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-function init() {
+function addRisposta(){
+  var risposta = $('.template-risposta > p').clone();
+  risposta.addClass('stilerisposta');
+  var target = $('.messaggi').append(risposta);
+}
+function addMessage(){
+  var message =$('#barraInvio').val();
+  var messaggio = $('.template-messaggio > p').clone();
+  messaggio.addClass('stilemessaggio').text(message);
+  var target = $('.messaggi').append(messaggio);
+}
+function pressInvio (){
+  if (event.which == 13) {
+    addMessage();
+    $('#barraInvio').val("");
+    setTimeout(function(){
+      addRisposta();
+    },1000);
+  }
+}
 
+function init() {
+  $(document).keydown(pressInvio);
 }
 $(document).ready(init);
