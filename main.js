bb92277db618959ad54fa35c5f699e528f8c5b28
Enrico <enricosellanora96@gmail.com>
2020-07-13 17:27:45 +0200

    :

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
+/*ricerca user*/
+/*function pressInvioRicerca(){
+  if (event.which == 13) {
+    ricercaPerNome();
+  }
+}*/
+/*function ricercaPerNome(){
+  var input;
+  var filtro;
+  var lista;
+  var voci;
+  var x;
+  var i;
+  var testo;
+  input = document.getElementById("barraRicerca");
+  filtro = input.value.toUpperCase();
+  lista = document.getElementById("contatti");
+  classi =lista.getElementsByClassName("contatto");
+  voci = lista.getElementsByTagName("h4");
+  for (i = 0; i < voci.length; i++) {
+    x = lista.getElementsByTagName("h4")[i];
+    testo = x.textContent || x.innerText;
+    if (testo.toUpperCase().indexOf(filtro) > -1) {
+      voci[i].style.display = "";
+    } else {
+      classi[i].style.display = "none";
+    }
+  }
+  if (input == "") {
+    classi.style.display = "";
+  }
+}*/
 
+
+
+/*invio e ricezioni messaggi*/
 function addRisposta(){
   var risposta = $('.template-risposta > p').clone();
   risposta.addClass('stilerisposta');
   var target = $('.messaggi').append(risposta);
 }
-
 function addMessage(){
   var message =$('#barraInvio').val();
   var messaggio = $('.template-messaggio > p').clone();
   messaggio.addClass('stilemessaggio').text(message);
   var target = $('.messaggi').append(messaggio);
 }
-
-function pressInvio (){
+function pressInvioMsg (){
   if (event.which == 13) {
     addMessage();
     $('#barraInvio').val("");
@@ -21,8 +54,11 @@ function pressInvio (){
     },1000);
   }
 }
+/*invio e ricezioni messaggi*/
+
 
 function init() {
-  $(document).keydown(pressInvio);
+/*  $('#barraRicerca').keydown(pressInvioRicerca);*/
+  $('#barraInvio').keydown(pressInvioMsg);
 }
 $(document).ready(init);
