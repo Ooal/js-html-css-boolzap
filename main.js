771f14eb938eb79533e8696b75a6a863d368500e
Enrico <enricosellanora96@gmail.com>
2020-07-13 16:14:02 +0200

    50px;

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+
 function addRisposta(){
   var risposta = $('.template-risposta > p').clone();
   risposta.addClass('stilerisposta');
   var target = $('.messaggi').append(risposta);
 }
+
 function addMessage(){
   var message =$('#barraInvio').val();
   var messaggio = $('.template-messaggio > p').clone();
   messaggio.addClass('stilemessaggio').text(message);
   var target = $('.messaggi').append(messaggio);
 }
+
 function pressInvio (){
   if (event.which == 13) {
     addMessage();
